28553c6f1b1cc1ca267ebaf532a42fc38b6f5e79
Trigary <trigary0@gmail.com>
2019-05-06 10:59:20 +0200

    weighted random javadocs

--- src/main/java/hu/trigary/tribukkit/random/WeightedRandom.java
+++ src/main/java/hu/trigary/tribukkit/random/WeightedRandom.java
@@ -9,8 +9,28 @@ import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 
+/**
+ * A collection of methods which can randomly select a single entry from a collection of entry-weight pairs.
+ * This class is preferred over {@link WeightedRandomCollection}
+ * when only a single entry is needed from the same source for performance reasons.
+ * <br><br>
+ * The entries can be null and the same entry can be present multiple times in the source.
+ * The weights must be non-null positive (non-zero and non-negative) values.
+ */
 public class WeightedRandom {
 	
+	/**
+	 * Randomly selects a single entry from the source, while taking the weights into consideration.
+	 * All weights are guaranteed to be acquired (via the {@code weightExtractor}), but not all entries:
+	 * {@code entryExtractor} might not be called for each element of {@code source}.
+	 *
+	 * @param source the source of the entries and weights
+	 * @param entryExtractor the function which gets an entry from a source element
+	 * @param weightExtractor the function which gets a weight from a source element
+	 * @param <T> the type of the source elements
+	 * @param <E> the type of the entry
+	 * @return the randomly selected entry
+	 */
 	@Contract(pure = true)
 	public static <T, E> E get(@NotNull Collection<T> source,
 			@NotNull Function<T, E> entryExtractor, @NotNull Function<T, Double> weightExtractor) {
@@ -28,12 +48,29 @@ public class WeightedRandom {
 		return result;
 	}
 	
+	/**
+	 * Randomly selects a single entry from the source, while taking the weights into consideration.
+	 * {@code weightExtractor} is guaranteed to be called for each source element.
+	 *
+	 * @param source the entries themselves and the source of the weights
+	 * @param weightExtractor the function which gets a weight from a source element
+	 * @param <E> the type of the entry
+	 * @return the randomly selected entry
+	 */
 	@Contract(pure = true)
 	public static <E> E get(@NotNull Collection<E> source,
 			@NotNull Function<E, Double> weightExtractor) {
 		return get(source, Function.identity(), weightExtractor);
 	}
 	
+	/**
+	 * Randomly selects a single entry (key) from the source,
+	 * while taking the weights (values) into consideration.
+	 *
+	 * @param source the map containing the entry-weight pairs
+	 * @param <E> the type of the entry
+	 * @return the randomly selected entry
+	 */
 	@Contract(pure = true)
 	public static <E> E get(@NotNull Map<E, Double> source) {
 		return get(source.entrySet(), Map.Entry::getKey, Map.Entry::getValue);
--- src/main/java/hu/trigary/tribukkit/random/WeightedRandomCollection.java
+++ src/main/java/hu/trigary/tribukkit/random/WeightedRandomCollection.java
@@ -8,10 +8,30 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 
+/**
+ * A collection which can retrieve random entries from a collection of entry-weight pairs.
+ * This class is preferred over {@link WeightedRandom}
+ * when multiple random entries are needed from the same source for performance reasons.
+ * <br><br>
+ * The entries can be null and the same entry can be present multiple times in the source.
+ * The weights must be non-null positive (non-zero and non-negative) values.
+ *
+ * @param <E> the type of the entry
+ */
 public class WeightedRandomCollection<E> {
 	private final NavigableMap<Double, E> entries = new TreeMap<>();
 	private final double weightSum;
 	
+	/**
+	 * Constructs a new collection from the specified source.
+	 * All weights are guaranteed to be acquired (via the {@code weightExtractor}), but not all entries:
+	 * {@code entryExtractor} might not be called for each element of {@code source}.
+	 *
+	 * @param source the source of the entries and weights
+	 * @param entryExtractor the function which gets an entry from a source element
+	 * @param weightExtractor the function which gets a weight from a source element
+	 * @param <T> the type of the source elements
+	 */
 	public <T> WeightedRandomCollection(@NotNull Collection<T> source,
 			@NotNull Function<T, E> entryExtractor, @NotNull Function<T, Double> weightExtractor) {
 		Validate.isTrue(!source.isEmpty());
@@ -25,22 +45,44 @@ public class WeightedRandomCollection<E> {
 		weightSum = sum;
 	}
 	
+	/**
+	 * Constructs a new collection from the specified source.
+	 * {@code weightExtractor} is guaranteed to be called for each source element.
+	 *
+	 * @param source the entries themselves and the source of the weights
+	 * @param weightExtractor the function which gets a weight from a source element
+	 */
 	public WeightedRandomCollection(@NotNull Collection<E> source,
 			@NotNull Function<E, Double> weightExtractor) {
 		this(source, Function.identity(), weightExtractor);
 	}
 	
+	/**
+	 * Constructs a new collection from the specified source.
+	 *
+	 * @param source the map containing the entry-weight pairs
+	 */
 	public WeightedRandomCollection(@NotNull Map<E, Double> source) {
 		this(source.entrySet(), Map.Entry::getKey, Map.Entry::getValue);
 	}
 	
 	
 	
+	/**
+	 * Gets a random entry from this collection, while taking the weights into consideration.
+	 *
+	 * @return the randomly selected entry
+	 */
 	@Contract(pure = true)
 	public E getRandom() {
 		return entries.floorEntry(ThreadLocalRandom.current().nextDouble(weightSum)).getValue();
 	}
 	
+	/**
+	 * Gets an unmodifiable collection containing all stored entries.
+	 *
+	 * @return the immutable collection of entries
+	 */
 	@NotNull
 	@Contract(pure = true)
 	public Collection<E> getEntries() {
