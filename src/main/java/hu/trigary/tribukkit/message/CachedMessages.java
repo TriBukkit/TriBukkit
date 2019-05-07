b140cf310ca0888c26f575ebfefafb482aa56053
Trigary <trigary0@gmail.com>
2019-05-07 09:00:52 +0200

    messages update & javadocs

--- src/main/java/hu/trigary/tribukkit/message/CachedMessages.java
+++ src/main/java/hu/trigary/tribukkit/message/CachedMessages.java
@@ -8,13 +8,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+/**
+ * An alternative {@link Messages} implementation that caches the output of the message source.
+ * This way eg. colors are only translated once.
+ */
 public class CachedMessages extends Messages {
 	private final Map<String, String> cache = new HashMap<>();
 	
+	/**
+	 * Creates a new instance with the specified message source.
+	 * The message source can take null and return null.
+	 *
+	 * @param messageSource the function which produces the messages based on the input
+	 */
 	public CachedMessages(@NotNull Function<String, String> messageSource) {
 		super(messageSource);
 	}
 	
+	/**
+	 * Creates a new instance which has a {@link ConfigurationSection} as its message source.
+	 *
+	 * @param translateColors whether the messages should be colorized
+	 * @param section the section containing the messages
+	 */
 	public CachedMessages(boolean translateColors, @NotNull ConfigurationSection section) {
 		super(translateColors, section);
 	}
--- src/main/java/hu/trigary/tribukkit/message/Messages.java
+++ src/main/java/hu/trigary/tribukkit/message/Messages.java
@@ -10,39 +10,86 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
 
+/**
+ * A class designed to make getting messages (especially ones with placeholders) easier.
+ * The source of these messages are configurable, but in most cases they are a {@link ConfigurationSection}.
+ * An alternative implementation of this class is {@link CachedMessages},
+ * which caches the output of the message source,
+ * this way eg. colors are only translated once.
+ */
 public class Messages {
 	private final Function<String, String> messageSource;
 	
+	/**
+	 * Creates a new instance with the specified message source.
+	 * The message source can take null and return null.
+	 *
+	 * @param messageSource the function which produces the messages based on the input
+	 */
 	public Messages(@NotNull Function<String, String> messageSource) {
 		this.messageSource = messageSource;
 	}
 	
+	/**
+	 * Creates a new instance which has a {@link ConfigurationSection} as its message source.
+	 *
+	 * @param translateColors whether the messages should be colorized
+	 * @param section the section containing the messages
+	 */
 	public Messages(boolean translateColors, @NotNull ConfigurationSection section) {
 		this(translateColors ? section::getString : key -> color(section.getString(key)));
 	}
 	
 	
 	
+	/**
+	 * Gets a {@link String} using the specified input from the message source.
+	 *
+	 * @param input the input based on which the message source produces the output
+	 * @return the {@link String} produced by the message source
+	 */
 	@Contract(pure = true)
 	public String get(String input) {
 		return messageSource.apply(input);
 	}
 	
+	/**
+	 * Calls {@link #get(String)} internally and then replaces the
+	 * placeholders according to {@link #format(String, String...)}.
+	 *
+	 * @param input the input based on which the message source produces its output
+	 * @param placeholders the placeholder-replacements pairs
+	 * @return the message with the placeholders replaced
+	 */
 	@Contract(pure = true)
 	public String get(String input, @NotNull String... placeholders) {
-		for (int i = 0; i < placeholders.length; ) {
-			input = StringUtils.replaceOnce(get(input), placeholders[i++], placeholders[i++]);
-		}
-		return input;
+		return format(get(input), placeholders);
 	}
 	
+	/**
+	 * Calls {@link #get(String)} internally and then replaces the
+	 * placeholder according to {@link #format(String, String, Object)}.
+	 *
+	 * @param input the input based on which the message source produces its output
+	 * @param placeholder the text that should be replaced
+	 * @param value the object that should replace the text
+	 * @return the message with the specified placeholder replaced
+	 */
 	@Contract(pure = true)
 	public String get(String input, @NotNull String placeholder, @NotNull Object value) {
-		return StringUtils.replaceOnce(get(input), placeholder, value.toString());
+		return format(get(input), placeholder, value);
 	}
 	
 	
 	
+	/**
+	 * Translates the color codes of the input.
+	 * Same as calling {@code ChatColor.translateAlternateColorCodes('&', input)},
+	 * except that null values are allowed (and produce null results).
+	 *
+	 * @param input the text to colorize
+	 * @return the colorized text or null, if the input was null
+	 */
 	@Contract(pure = true, value = "null -> null; !null -> !null")
 	public static String color(@Nullable String input) {
 		return input == null ? null : ChatColor.translateAlternateColorCodes('&', input);
@@ -50,6 +97,47 @@ public class Messages {
 	
 	
 	
+	/**
+	 * Replaces the specified placeholders with the specified values in the specified input.
+	 * The {@code placeholders} must contain the placeholder text
+	 * and then its replacement directly after each other.
+	 *
+	 * @param input the text that should have its placeholders replaced
+	 * @param placeholders the placeholder-replacements pairs
+	 * @return the input with the placeholders replaced
+	 */
+	@Contract(pure = true, value = "null, _ -> null; !null, _ -> !null")
+	public static String format(String input, @NotNull String... placeholders) {
+		Validate.isTrue(placeholders.length % 2 == 0, "Count of placeholders and replacements must match");
+		for (int i = 0; i < placeholders.length; ) {
+			input = StringUtils.replaceOnce(input, placeholders[i++], placeholders[i++]);
+		}
+		return input;
+	}
+	
+	/**
+	 * Replaces the specified placeholder with the specified value in the specified input.
+	 *
+	 * @param input the text that should have its placeholder replaced
+	 * @param placeholder the text that should be replaced
+	 * @param value the object that should replace the text
+	 * @return the input with the specified placeholder replaced
+	 */
+	@Contract(pure = true, value = "null, _, _ -> null; !null, _, _ -> !null")
+	public static String format(String input, @NotNull String placeholder, @NotNull Object value) {
+		return StringUtils.replaceOnce(input, placeholder, value.toString());
+	}
+	
+	
+	
+	/**
+	 * Formats the specified duration to "Xd, Xh, etc."
+	 * An input of 0 outputs "0s".
+	 * Negative values are not allowed.
+	 *
+	 * @param seconds the duration to format
+	 * @return the formatted duration
+	 */
 	@NotNull
 	@Contract(pure = true)
 	public static String formatShortTime(long seconds) {
@@ -57,7 +145,7 @@ public class Messages {
 			return "0s";
 		}
 		
-		Validate.isTrue(seconds > 0, "Time must be positive");
+		Validate.isTrue(seconds > 0, "Time must be non-negative");
 		long minutes = seconds / 60;
 		seconds %= 60;
 		long hours = minutes / 60;
@@ -81,6 +169,14 @@ public class Messages {
 		return builder.toString().trim();
 	}
 	
+	/**
+	 * Formats the specified duration to "X days, X hours, etc."
+	 * An input of 0 outputs "0 seconds".
+	 * Negative values are not allowed.
+	 *
+	 * @param seconds the duration to format
+	 * @return the formatted duration
+	 */
 	@NotNull
 	@Contract(pure = true)
 	public static String formatLongTime(long seconds) {
@@ -88,7 +184,7 @@ public class Messages {
 			return "0 seconds";
 		}
 		
-		Validate.isTrue(seconds > 0, "Time must be positive");
+		Validate.isTrue(seconds > 0, "Time must be non-negative");
 		long minutes = seconds / 60;
 		seconds %= 60;
 		long hours = minutes / 60;
@@ -114,10 +210,22 @@ public class Messages {
 	
 	
 	
+	/**
+	 * Formats the specified decimal number, keeping 2 decimal places.
+	 *
+	 * @param number the number to format
+	 * @return the formatted number
+	 */
 	public static String formatDecimal(float number) {
 		return formatDecimal((double) number);
 	}
 	
+	/**
+	 * Formats the specified decimal number, keeping 2 decimal places.
+	 *
+	 * @param number the number to format
+	 * @return the formatted number
+	 */
 	public static String formatDecimal(double number) {
 		return String.format("%.2f", number);
 	}
